287f5628207da8df8e2aef7c00e8adf103f84126
Ashwin <ashwin.s@gmail.com>
2024-03-31 15:12:38 +0530

    feat: added firebase

---
+++ src/utils/firebase.ts
@@ -0,0 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const {
+  REACT_APP_FIREBASE_API_KEY: apiKey,
+  REACT_APP_FIREBASE_AUTH_DOMAIN: authDomain,
+  REACT_APP_FIREBASE_PROJECT_ID: projectId,
+  REACT_APP_FIREBASE_STORAGE_BUCKET: storageBucket,
+  REACT_APP_FIREBASE_MESSAGING_SENDER_ID: messagingSenderId,
+  REACT_APP_FIREBASE_APP_ID: appId,
+  REACT_APP_FIREBASE_MEASUREMENT_ID: measurementId,
+} = process.env;
+
+const firebaseConfig = {
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+export const auth = getAuth();
